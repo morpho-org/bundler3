f81cced5fbb451b2768c793ec1906cebf3f5a974
Adrien Husson <adhusson@gmail.com>
2024-09-12 16:53:33 +0200

    docs: fix natspec title in IMorphoBundlerModule

--- src/interfaces/IMorphoBundlerModule.sol
+++ src/interfaces/IMorphoBundlerModule.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity >=0.5.0;
 
-/// @title IMorphoBundler
+/// @title IMorphoBundlerModule
 /// @author Morpho Labs
 /// @custom:contact security@morpho.org
 /// @notice Interface of Morpho Bundler module.
