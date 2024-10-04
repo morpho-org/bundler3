c7311ed44fb7e2ba0a7314cdad8fc1c6a6299a50
Adrien Husson <adhusson@gmail.com>
2024-10-04 16:28:49 +0200

    chore: move to 0.8.27

--- src/UrdBundler.sol
+++ src/UrdBundler.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity 0.8.26;
+pragma solidity 0.8.27;
 
 import {IUniversalRewardsDistributor} from
     "../lib/universal-rewards-distributor/src/interfaces/IUniversalRewardsDistributor.sol";
