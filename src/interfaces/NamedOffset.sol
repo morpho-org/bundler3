eb6b28208261c4f226ad8e7549804dca5c48bfc0
Adrien Husson <adhusson@gmail.com>
2024-10-04 16:35:23 +0200

    docs: document NamedOffset struct

--- src/interfaces/NamedOffset.sol
+++ src/interfaces/NamedOffset.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity >= 0.7.0;
 
+/// @notice Struct that represents:
+/// - a memory offset, together with
+/// - a name for the value at that offset
 struct NamedOffset {
     bytes32 name;
     uint256 offset;
