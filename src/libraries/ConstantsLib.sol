6741f44a60c703e89796f20461ee771b40d84acd
Adrien Husson <adhusson@gmail.com>
2024-10-07 11:11:00 +0200

    Update src/libraries/ConstantsLib.sol
    Signed-off-by: Adrien Husson <adhusson@gmail.com>

--- src/libraries/ConstantsLib.sol
+++ src/libraries/ConstantsLib.sol
@@ -4,6 +4,6 @@ pragma solidity ^0.8.0;
 /// @dev The default value of the initiator of the multicall transaction is not the address zero to save gas.
 address constant UNSET_INITIATOR = address(1);
 
-/// @dev Slot where currently called bundler module is transiently stored.
+/// @dev Slot where the currently called bundler module is transiently stored.
 /// @dev Equal to keccak256("Morpho Bundler Current Module Slot"), stored as hex literal so it can be used in assembly.
 bytes32 constant CURRENT_MODULE_SLOT = 0x4a208b9e5a8db61bf46ae647a7eb221065bc0d8fcb9958db28b558743ea1472d;
