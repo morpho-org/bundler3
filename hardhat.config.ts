78735547873312f8e357f97521d76b00cff15127
MathisGD <mathis@morpho.xyz>
2024-10-07 14:50:27 +0200

    chore: fix and simplify Ci

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -36,7 +36,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.26",
+        version: "0.8.27",
         settings: {
           optimizer: {
             enabled: true,
