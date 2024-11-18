77d479a626f47620d080c22c475e19848e38a796
MathisGD <mathis@morpho.xyz>
2024-11-18 17:15:52 +0100

    docs: minor fix

--- src/migration/CompoundV2MigrationModule.sol
+++ src/migration/CompoundV2MigrationModule.sol
@@ -68,7 +68,7 @@ contract CompoundV2MigrationModule is BaseModule {
     /// @notice Redeems cToken from CompoundV2.
     /// @dev cTokens must have been previously sent to the module.
     /// @param cToken The address of the cToken contract
-    /// @param amount The amount of cToken to redeem. Pass 2^256-1 to redeem the module's `cToken` balance.
+    /// @param amount The amount of cToken to redeem. Pass 2^256-1 to redeem the module's balance.
     /// @param receiver The account receiving the redeemed assets.
     function compoundV2Redeem(address cToken, uint256 amount, address receiver) external bundlerOnly {
         if (amount == type(uint256).max) amount = ICToken(cToken).balanceOf(address(this));
