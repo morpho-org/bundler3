f43015a2ed2e20d5de75b4d9691d196d751d8eee
MathisGD <74971347+MathisGD@users.noreply.github.com>
2024-11-20 13:29:46 +0100

    docs: minor update
    Signed-off-by: MathisGD <74971347+MathisGD@users.noreply.github.com>

--- src/migration/AaveV2MigrationModule.sol
+++ src/migration/AaveV2MigrationModule.sol
@@ -34,7 +34,7 @@ contract AaveV2MigrationModule is BaseModule {
     /// @notice Repays the initiator's debt on AaveV2.
     /// @dev Underlying tokens must have been previously sent to the module.
     /// @param token The address of the token to repay.
-    // / @param amount The amount of `token` to repay. Pass `type(uint).max` to repay the maximum repayable debt
+    /// @param amount The amount of `token` to repay. Pass `type(uint).max` to repay the maximum repayable debt
     /// (mininimum of the module's balance and the initiator's debt).
     /// @param interestRateMode The interest rate mode of the position.
     function aaveV2Repay(address token, uint256 amount, uint256 interestRateMode) external bundlerOnly {
