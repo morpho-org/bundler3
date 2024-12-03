e8462af93d0633c79aa96112eafd3f887b8f274f
MerlinEgalite <egalite.merlin@gmail.com>
2024-12-03 10:03:47 +0700

    style: remove useless imports

--- src/EthereumModule1.sol
+++ src/EthereumModule1.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.28;
 
-import {IDaiPermit} from "./interfaces/IDaiPermit.sol";
 import {IWstEth} from "./interfaces/IWstEth.sol";
 import {IStEth} from "./interfaces/IStEth.sol";
 
