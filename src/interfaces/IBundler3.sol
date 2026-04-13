54d7e6b4b930e55dc0accc255ed9a42d28de4f6b
Quentin Garchery <garchery.quentin@gmail.com>
2026-04-13 18:13:04 +0200

    fix wording

--- src/interfaces/IBundler3.sol
+++ src/interfaces/IBundler3.sol
@@ -3,8 +3,8 @@ pragma solidity >=0.8.0;
 
 /// @notice Struct containing all the data needed to make a call.
 /// @notice The call target is `to`, the calldata is `data` with value `value`.
-/// @notice If `skipRevert` is true, other planned calls will continue executing even if this call reverts. `skipRevert`
-/// will ignore all reverts types of the call. Use with caution.
+/// @notice If `skipRevert` is true, the fact that the call reverts will not cause the whole bundle to revert. Use with
+/// caution, since all reverts are ignored.
 /// @notice If the call will trigger a reenter, the callbackHash should be set to the hash of the reenter bundle data.
 /// @notice If callbackHash is set and the call reverts, the whole bundle will revert even if `skipReverts` is true.
 struct Call {
